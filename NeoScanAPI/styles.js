ec48d6a9c7fcb9e645f33e2b591c588d90d460c8
webberic92 <36771546+webberic92@users.noreply.github.com>
2019-04-26 14:08:40 -0500

    Add files via upload
    Centered table with some additional CSS.
    
    TODO : input validation....

--- NeoScanAPI/styles.js
+++ NeoScanAPI/styles.js
@@ -14,25 +14,30 @@ function myAddress() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-//      document.getElementById("demo").innerHTML =
-//      this.responseText;
-    	//here we have json string, need to convert to objet
         let jsonObj=JSON.parse(this.responseText);
 
      // traverse the JSON object
       //WORKS
-       var balance =jsonObj['balance'];     
-       for (var i=0; i<balance.length; i++) {   
-      document.getElementById('demo1').innerHTML += "<br>" +  JSON.stringify(balance[i].asset_symbol);
-      }
-      
-       for (var i=0; i<balance.length; i++) {   
-      document.getElementById('demo2').innerHTML += "<br>" + JSON.stringify(balance[i].amount);  
-        
-       }}
-      else if( this.readyState == 4 && this.status == 400) {
-          document.getElementById("demo").innerHTML = "That is an incorrect wallet address format...."
-  }  
+       var balance =jsonObj['balance'];
+       //creates table
+ 	  var table="<tr><th>Asset ID</th><th>Amount</th></tr>";
+ 	  //loops table
+ 	  for (var i=0; i<balance.length; i++) {  
+    	   table += "<tr><td>" +
+   	    JSON.stringify(balance[i].asset_symbol) +
+   	    "</td><td>" +
+   	    JSON.stringify(balance[i].amount) +
+   	    "</td></tr>";
+       }
+       document.getElementById("demoTable").innerHTML = table;
+       
+       }
+     
+    if( this.readyState == 4 && this.status == 400) {
+        document.getElementById("demo1").innerHTML = "That is an incorrect wallet address format....";
+
+    }  
+     
   };
   xhttp.open("GET", "https://api.neoscan.io/api/main_net/v1/get_balance/" + x, true);
   xhttp.send();
@@ -47,15 +52,20 @@ function myAddress() {
 function clearAddress() {
 	  document.getElementById("displayAddress").innerHTML = "Address Cleared, Please enter another...";
 	  document.getElementById("youChose").innerHTML = "";
-//	  document.getElementById("demo").innerHTML = "";
+	  document.getElementById("demoTable").innerHTML = "";
 	  document.getElementById("demo1").innerHTML = "";
 	  document.getElementById("demo2").innerHTML = "";
+	  table= "";
+      
+	  var y = document.getElementById("horizontalLine");
+	  y.style.display = "none";
+	  
+	  var t = document.getElementById("demoTable");
+	  t.style.display = "none";
+
 
 	  var x = document.getElementById("myAddress").value;
 	  x=null;
-	  
-	  var y = document.getElementById("horizontalLine");
-	  y.style.display = "none";
 	}
 
 
